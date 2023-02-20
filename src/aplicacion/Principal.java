882c6c71a53d1b9d0ebc926020eb82d1676d4082
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:04:51 +0100

    ej2 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,21 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_2;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num;
+
+        System.out.println("Introduce un numero");
+        num=sc.nextInt();
+
+        System.out.println("El factorial de "+num+" es "+ ej2_2.fac(num));
+
+
+
+
+
+
+    }
 }
--- src/dominio/ej2_2.java
+++ src/dominio/ej2_2.java
@@ -1,2 +1,15 @@
-package dominio;public class ej2_2 {
+package dominio;
+
+public class ej2_2 {
+
+
+    public static int fac(int n) {
+        if(n<=1) {
+            return 1;
+        }else {
+            return n*fac(n-1);
+        }
+    }
+
 }
+
